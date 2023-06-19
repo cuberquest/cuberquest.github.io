a6ad42347a8394b9d8ee500a1ba153501b891d1f
ws-testing <134105195+WS-Testing@users.noreply.github.com>
2023-06-19 13:02:33 -0400

    Another change.

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-document.body.addEventListener("load", () => alert());
+document.body.addEventListener("load", () => {
+  alert("hello");
+  document.getElementsByTagName("p")[0].innerHTML = "hello";
+});
