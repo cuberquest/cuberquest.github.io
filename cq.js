13e747bf0dbb02f38b955dc15d28fd70a5923b35
ws-testing <134105195+cq-testdev@users.noreply.github.com>
2023-09-22 17:38:00 -0400

    Starting the Canvas scripts

---
+++ cq.js
@@ -0,0 +1,69 @@
+let myGamePiece,
+mySound,
+myMusic;
+
+function loadGame() {
+  myGamePiece = new comp(100, 100, "", undefined, undefined, "img", true)
+  myGameArea.start();
+}
+
+const myGameArea = {
+  canvas: document.createElement("canvas"),
+  start: function() {
+    this.canvas.width = innerWidth;
+    this.canvas.height = innerHeight;
+    this.context = this.canvas.getContext("2d");
+    document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+    this.canvas.innerHTML = "";
+    this.frameNo = 0;
+    this.interval = setInterval(updateGameArea, 1000 / 1000);
+  },
+  clear: function() {
+    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height)
+  },
+  stop: function() {
+    clearInterval(this.interval)
+  }
+}
+
+function updateSize() {
+
+  updateGameArea();
+}
+updateSize();
+
+class comp {
+  constructor(width, height, color, x, y, type, isPlayer = false) {
+    this.type = type;
+    this.width = width;
+    this.height = height;
+    this.x = x;
+    this.y = y;
+    this.speedX = 0;
+    this.speedY = 0;
+    this.update = function() {}
+    this.newPos = function() {}
+  }
+}
+
+class sound {
+  constructor(src) {
+    this.sound = document.createElement("audio");
+    this.sound.src = src;
+    this.sound.setAttribute("preload", "auto");
+    this.sound.setAttribute("controls", "none");
+    this.sound.style.display = "none";
+    document.body.appendChild(this.sound);
+    this.play = function() {
+      this.sound.play();
+    };
+    this.stop = function() {
+      this.sound.pause();
+    };
+  }
+}
+
+function updateGameArea() {
+  myGameArea.clear();
+  window.myGameArea.frameNo += 1;
+}
