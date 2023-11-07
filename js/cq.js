df1d2b4934883c4013ef603b0fb6ff6892b5102c
cuberquest <134105195+cuberquest@users.noreply.github.com>
2023-11-07 16:53:47 -0500

    Changing setInterval to requestAnimationFrame for smoother animations.

--- js/cq.js
+++ js/cq.js
@@ -25,7 +25,7 @@ const myGameArea = {
     document.body.insertBefore(this.canvas, document.body.childNodes[0]);
     this.canvas.innerHTML = "";
     this.frameNo = 0;
-    this.interval = setInterval(updateGameArea, 1000 / 1000);
+    this.interval = requestAnimationFrame(updateGameArea);
   },
   clear: function() {
     this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
