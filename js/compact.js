93466c42567a0d860d159eefd236e195b0b65806
cuberquest <134105195+cuberquest@users.noreply.github.com>
2023-11-07 16:26:00 -0500

    Testing compact function.

--- js/compact.js
+++ js/compact.js
@@ -1,1 +1,16 @@
-export function format(n){var x=Number(n);if(x<1e3){return x}else if(x>=1e3&&x<1e6){return Math.floor(x/1e3*10)/10+"k"}else if(x>=1e6&&x<1e9){return Math.floor(x/1e6*10)/10+"m"}else if(x>=1e9&&x<1e12){return Math.floor(x/1e9*10)/10+"b"}else if(x>=1e12&&x<1e15){return Math.floor(x/1e12*10)/10+"t"}else if(x>=1e15){return Math.floor(x/1e15*10)/10+"q"}}
+export default function format(n) {
+  let x = Number(n);
+  if (x < 1e3) {
+    return x;
+  } else if (x >= 1e3 && x < 1e6) {
+    return Math.floor(x / 1e3 * 10) / 10 + "k";
+  } else if (x >= 1e6 && x < 1e9) {
+    return Math.floor(x / 1e6 * 10) / 10 + "m";
+  } else if (x >= 1e9 && x < 1e12) {
+    return Math.floor(x / 1e9 * 10) / 10 + "b";
+  } else if (x >= 1e12 && x < 1e15) {
+    return Math.floor(x / 1e12 * 10) / 10 + "t";
+  } else if (x >= 1e15) {
+    return Math.floor(x / 1e15 * 10) / 10 + "q";
+  }
+}
--- js/cq.js
+++ js/cq.js
@@ -28,7 +28,7 @@ const myGameArea = {
     this.interval = setInterval(updateGameArea, 1000 / 1000);
   },
   clear: function() {
-    //this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
   },
   stop: function() {
     clearInterval(this.interval);
